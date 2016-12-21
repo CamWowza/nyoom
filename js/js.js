db1b28b96ba27f6cc6ecc352c1b5d2a6d2bfe018
JaydenKimberlin <paperj7201@gmail.com>
2016-12-21 18:03:28 -0500

    Changed what the a tag does.
    Made it a window.open function to open the links in new tabs, rather
    than leaving the page entirely.

--- js/js.js
+++ js/js.js
@@ -1,4 +1,11 @@
-var nyoom= getElementById('nyoom');
+// var audio = getElementById('audio') This is the audio variable.;
 function nyoom(){
 	alert("Cameron Says Nyoom");
+	// audio.play() This is the audio playback;
+}
+function camwow(){
+	window.open('https://camwowza.github.io/camwowphoto');
+}
+function jkdesign(){
+	window.open('https://jaydenk4201.github.io')
 }
